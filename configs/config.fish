a845bc7de2e072ca942c33f04c9c36075815f247
Carlo Zottmann <carlo@municode.de>
2015-01-26 13:09:37 +0100

    [CHG] Enabled colored output in 'ls'.

--- configs/config.fish
+++ configs/config.fish
@@ -31,7 +31,7 @@ end
 # ### Aliases
 alias gpull "git pull origin"
 alias gpush "git push origin"
-alias ls "ls -al"
+alias ls "ls -Gal"
 alias tmux "env TERM=xterm-256color tmux"
 alias v "vagrant"
 
